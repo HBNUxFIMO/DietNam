95b0730d368c9798954c103f75dc5a96fff9452f
giljihun <accc45@outlook.com>
2024-08-19 10:05:48 +0700

    Add Pods directory to .gitignore

---
+++ DietNam/DietNam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DietNam
+//
+//  Created by 길지훈 on 8/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
